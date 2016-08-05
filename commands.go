1e8681806da5c7f52c97b7c2896ca25592ea422f
sunxia <sunxia@sunxia-PC.local>
2016-08-05 11:04:32 +0800

    add skeleton code

---
+++ commands.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/mitchellh/cli"
+	"github.com/asdfsx/golang_skeleton/command"
+)
+
+// Commands is the mapping of all the available goconnector commands.
+var Commands map[string]cli.CommandFactory
+
+func init() {
+	ui := &cli.BasicUi{Writer: os.Stdout}
+
+	Commands = map[string]cli.CommandFactory{
+		"version": func() (cli.Command, error) {
+			ver := Version
+			rel := VersionPrerelease
+			if GitDescribe != "" {
+				ver = GitDescribe
+			}
+			if GitDescribe == "" && rel == "" {
+				rel = "dev"
+			}
+
+			return &command.VersionCommand{
+				Revision:          GitCommit,
+				Version:           ver,
+				VersionPrerelease: rel,
+				Ui:                ui,
+			}, nil
+		},
+	}
+}
+
+// makeShutdownCh returns a channel that can be used for shutdown
+// notifications for commands. This channel will send a message for every
+// interrupt or SIGTERM received.
+func makeShutdownCh() <-chan struct{} {
+	resultCh := make(chan struct{})
+
+	signalCh := make(chan os.Signal, 4)
+	signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		for {
+			<-signalCh
+			resultCh <- struct{}{}
+		}
+	}()
+
+	return resultCh
+}
